ebade5674383f0eb055e7a19c5116bd34dc23e36
fgr <107344363+frg01@users.noreply.github.com>
2024-03-21 20:19:43 +0800

    Add files via upload

---
+++ src/database/base.go
@@ -0,0 +1,20 @@
+package database
+
+// 定义一个接口
+type Base interface {
+}
+
+func init() {
+}
+
+func insertData(data string) {
+}
+
+func insertDataMutil() {
+}
+
+func createIndex(data string) {
+}
+
+func singleSearch() {
+}
