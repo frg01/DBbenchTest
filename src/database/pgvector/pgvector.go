bceda00769eff75d518784f982ebd8cfa320262c
fgr <107344363+frg01@users.noreply.github.com>
2024-03-21 20:18:50 +0800

    Add files via upload

---
+++ src/database/pgvector/pgvector.go
@@ -0,0 +1,19 @@
+package pgvector
+
+type Pgvector struct {
+}
+
+func init() {
+}
+
+func insertData(data string) {
+}
+
+func insertDataMutil() {
+}
+
+func createIndex(data string) {
+}
+
+func singleSearch() {
+}
